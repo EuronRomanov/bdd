29513065ffbdf6b80fa00de06efc7b39c4590598
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-03-13 18:53:05 -0500

    reto 23

---
+++ tipos_relaciones/inserciones.sql
@@ -0,0 +1,23 @@
+--cuentas 
+insert into usuario values ('1303753618','PABLO MARCELO','ABAD NIETO','AHORROS',200.80);
+insert into usuario values ('1706172648','CARLOS ENRIQUE','ABRIL ABRIL','CORRIENTE',1000.78);
+insert into usuario values ('0100967652','NARCISA DEL LOURDES','ACARO CASTILLO','AHORROS',5000.10);
+insert into usuario values ('1103037048','SONIA CECILIA','ACEVEDO PALACIO','CORRIENTE',2000.78);
+insert into usuario values ('1704997012','DAVID JOSÉ','ACOSTA VÁSQUEZ','CORRIENTE',2000.78);
+insert into usuario values ('1714818299','ROSARIO CARLA','BERON PALOMEQUE','CORRIENTE',2000.70);
+insert into usuario values ('1713627071','MARIO ALBERTO','BLUM AGUIRRE','AHORROS',5400.10);
+insert into usuario values ('0200982163','FLAVIO GEOVANNY','BRAVO LUZURIAGA','AHORROS',6000.60);
+insert into usuario values ('1710027071','JOSE JOSE','BLUM AGUIRRE','AHORROS',5400.10);
+insert into usuario values ('0201112163','HERNA VICENTE','BARSALLO ROLDAN','AHORROS',6000.60);
+
+insert into cuentas values ('12345','1303753618',NOW(),134.7);
+insert into cuentas values ('12446','1706172648','2010-01-23',300);
+insert into cuentas values ('12006','0100967652','2010-10-01',400.78);
+insert into cuentas values ('12786','1103037048','2010-12-01',400.78);
+insert into cuentas values ('16886','1704997012','2010-10-02',480.08);
+insert into cuentas values ('18996','1714818299','2017-7-01',199.10);
+insert into cuentas values ('11116','1713627071','2018-9-01',1000.01);
+insert into cuentas values ('10989','0200982163','2017-10-01',12949.8);
+insert into cuentas values ('18999','1710027071','2020-8-01',556.10);
+insert into cuentas values ('18994','0201112163','2012-10-01',700.98);
+
